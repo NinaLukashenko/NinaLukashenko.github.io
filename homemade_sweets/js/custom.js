4448e1e476d64b69782f48339f0a7de2ec779471
NinaLukashenko <nina.lucashenco@gmail.com>
2018-11-12 20:11:49 +0200

    Homemade Sweets are completed

---
+++ homemade_sweets/js/custom.js
@@ -0,0 +1,9 @@
+function showMoreCakes() {
+	document.getElementById("additional-cakes").style.display = "block";
+	document.getElementById("button-showMoreCakes").style.display = "none";
+}
+
+function showMoreSweets() {
+	document.getElementById("additional-sweets").style.display = "block";
+	document.getElementById("button-showMoreSweets").style.display = "none";
+}
